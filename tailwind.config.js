e1276ac69c95ff5db0769fae513b675e3194eae3
Christian Wygoda <christian.wygoda@wygoda.net>
2024-10-05 20:39:17 +0200

    feat: add tailwind support

---
+++ tailwind.config.js
@@ -0,0 +1,8 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./content/**/*.{html,md}", "./themes/airy/**/*.{html,js}"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
